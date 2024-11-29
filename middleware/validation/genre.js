da89538b64b871e47e78d37f2b9243f2f4b8a652
Carlos Costa <helio_neon_argon@hotmail.com>
2024-11-29 17:54:07 +0000

    Versão 41

--- middleware/validation/genre.js
+++ middleware/validation/genre.js
@@ -4,3 +4,11 @@ exports.validateGenre = [
     check('name').trim().not().isEmpty().
     withMessage('Por favor deve indicar o género literário')
 ]
+
+exports.genreValidation = (req, res, next) => {
+    const result = validationResult(req).array()
+    if(!result.length) return next()
+
+    const error = result[0].msg
+    res.json({success: false, message: error})
+}
--- routes/genre.js
+++ routes/genre.js
@@ -2,13 +2,13 @@ const express = require('express')
 
 const genreRouter = express.Router()
 const {createGenre, deleteGenreById, getGenres, getGenreById, updateGenreById} = require('../controllers/genre')
-const {validateGenre} = require('../middleware/validation/genre')
+const {genreValidation, validateGenre} = require('../middleware/validation/genre')
 const { validationResult } = require('express-validator')
 
 genreRouter.delete('/genre/:id', deleteGenreById)
 genreRouter.get('/genres', getGenres)
 genreRouter.get('/genre/:id', getGenreById)
-genreRouter.patch('/genre/:id', updateGenreById)
-genreRouter.post('/create-genre', validateGenre, createGenre)
+genreRouter.patch('/genre/:id', validateGenre, genreValidation, updateGenreById)
+genreRouter.post('/create-genre', validateGenre, genreValidation, createGenre)
 
 module.exports = genreRouter

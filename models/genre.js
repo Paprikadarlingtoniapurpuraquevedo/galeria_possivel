a1f6faf2cf4eb7a2ce494b69b49d1d5ad5f53653
Carlos Costa <helio_neon_argon@hotmail.com>
2024-12-14 23:06:28 +0000

    Versão 56

--- models/genre.js
+++ models/genre.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 const { permission } = require('process')
 var Schema = mongoose.Schema
 var genreSchema = new Schema({
-    name: {
+    genre: {
         type: String,
         required: true
     },
